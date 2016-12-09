bd34234174fc1187ee7905ab7e9035762c640e80
JWTappert <tappertj@oregonstate.edu>
2016-12-08 20:15:13 -0800

    Added some more functions as well as permissions for the purge command

--- hackBot.js
+++ hackBot.js
@@ -1,25 +1,54 @@
-var Discord = require("discord.js");
-var bot = new Discord.Client();
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const config = require("./config.json");
 
 bot.on("message", msg => {
-	//This is the prefix for all commands
-	let prefix = "!";
 	//If a command doesn't begin with the prefix, return
-	if(!msg.content.startsWith(prefix)) return;
+	if(!msg.content.startsWith(config.prefix)) return;
 	//prevents bot-cepetion
 	if(msg.author.bot) return;
 
+	let command = msg.content.split(" ")[0];
+	command = command.slice(config.prefix.length);
+
+	let args = msg.content.split(" ").slice(1);
+
+	if(command === "say"){
+		msg.channel.sendMessage(args.join(" "));
+	}
+
+	if (command === "add"){
+		let numArray = args.map(n=> parseInt(n));
+		let total = numArray.reduce( (p, c) => p+c);
+		msg.channel.sendMessage(total);
+	}
+
 	//!rules command explaining the rules
-	if(msg.content.startsWith(prefix + "rules")){
-		msg.channel.sendMessage("Be nice and don't copy each other's homework!")
+	if(command === "rules"){
+		msg.channel.sendMessage("Be nice and don't copy each other's homework!");
+	}
+
+	//!xmas command
+	if(command === "xmas"){
+		msg.channel.sendMessage("https://giphy.com/gifs/foxhomeent-3o7TKLHb0PWRNnoVq0");
 	}
 
 	//!purge command deletes all messages in the channel it's called.
-	//if (message.content.startsWith(prefix + "purge")) {
-    //	let messagecount = parseInt(params[0]);
-    //	message.channel.fetchMessages({limit: messagecount})
-    //    	.then(messages => message.channel.bulkDelete(messages));
-	//}
+	if (command === "purge") {
+		let boardRole = msg.guild.roles.find("name", "Board Member");
+		if(msg.member.roles.has(boardRole.id)){
+    		let messagecount = 100;
+    		msg.channel.fetchMessages({limit: messagecount})
+       		.then(messages => msg.channel.bulkDelete(messages));
+       	} else {
+       		return msg.reply("sorry m8, you're not authorized to use that command.");
+       	}
+
+       	if(!msg.guild.member(bot.user).hasPermission("KICK_MEMBERS")){
+       		return msg.reply("sorry m8, I'm not authorized to use that command.");
+       	}
+
+	}
 });
 
 //Detects new users and sends them a little message.
@@ -36,4 +65,4 @@ bot.on("ready", () => {
 bot.on('error', e => { console.error(e); });
 
 //login creds
-bot.login('Token');
+bot.login(config.token);
