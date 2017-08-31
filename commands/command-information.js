f406633da977e6b36639d90c3d8eff1fae326854
Yong Bakos <ybakos@humanoriented.com>
2017-08-30 19:38:02 -0700

    Put commands in alphabetical order.

--- commands/command-information.js
+++ commands/command-information.js
@@ -1,47 +1,47 @@
 var commandInformation = [
   {
-      command: "help",
-      description: "Displays this message"
+    command: "add [Integers]",
+    description: "Adds together _integers_ passed as arguments."
   },
   {
-      command: "say [Phrase]",
-      description: "Echos back the string passed as arguments."
+    command: "gitProfile [Username]",
+    description: "Retrieves any public github profile.",
   },
   {
-      command: "lmgtfy [Search Term]",
-      description: "When someone is being...lazy...?"
+    command: "help",
+    description: "Displays this message"
   },
   {
-      command: "search [Search Term]",
-      description: "Searches the web for the passed query and return the top result."
+    command: "lmgtfy [Search Term]",
+    description: "When someone is being...lazy...?"
   },
   {
-      command: "add [Integers]",
-      description: "Adds together _integers_ passed as arguments."
+    command: "purge",
+    description: "Purges the channel it is called within. Restricted to Board Members and Administrators.",
   },
   {
-      command: "rules",
-      description: "List the rules for the CTC Discord server.",
+    command: "rules",
+    description: "List the rules for the CTC Discord server.",
   },
   {
-      command: "xmas",
-      description: "Merry Christmas, ya filthy animals.",
+    command: "say [Phrase]",
+    description: "Echos back the string passed as arguments."
   },
   {
-      command: "purge",
-      description: "Purges the channel it is called within. Restricted to Board Members and Administrators.",
+    command: "search [Search Term]",
+    description: "Searches the web for the passed query and return the top result."
   },
   {
-      command: "weather [Location]",
-      description: "Provide City and State, or City and Country to get current temperature.",
+    command: "source",
+    description: "Retrieves the hackBot's github repository.",
   },
   {
-      command: "source",
-      description: "Retrieves the hackBot's github repository.",
+    command: "weather [Location]",
+    description: "Provide City and State, or City and Country to get current temperature.",
   },
   {
-      command: "gitProfile [Username]",
-      description: "Retrieves any public github profile.",
+    command: "xmas",
+    description: "Merry Christmas, ya filthy animals.",
   }
 ]
 
--- commands/command.js
+++ commands/command.js
@@ -9,6 +9,48 @@ class Command {
         this.argsErrorMessage += "Refer to `!help` or ask an Adminstrator if this error occurs"
     }
 
+    add(args, msg) {
+      var { channel } = msg;
+      if ( args.length < 1) { return channel.sendMessage(this.argsErrorMessage); }
+      let numArray = args.map(n => parseInt(n));
+      let total = numArray.reduce((p, c) => p + c);
+      return channel.sendMessage(total);
+    }
+
+    gitProfile(args, msg) {
+        if ( args.length < 1) { return channel.sendMessage(this.argsErrorMessage); }
+        var getGithubProfile = (userName) => {
+                return new Promise((resolve, reject) => {
+                    if (userName === undefined) {
+                        return reject('Please enter a username.');
+                    } else {
+                        let url = `https://api.github.com/users/${userName}`;
+                        let options = {
+                            url: url,
+                            headers: {
+                                'User-Agent': 'request'
+                            }
+                        }
+                        request(options, (error, response, body) => {
+                            if (error) {
+                                return reject(`error: ${error}`);
+                            } else {
+                                resolve(body);
+                            }
+                        })
+                    }
+                })
+        }
+        getGithubProfile(args)
+            .then((profile) => {
+                let parsedProfile = JSON.parse(profile);
+                msg.channel.sendMessage(parsedProfile.html_url);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
     help(args, msg) {
         var helpMsg = "I am here to help! Well...mostly just make you chuckle at this point, let's be honest.\n\n";
         helpMsg += "Here is a list of the commands that we've got right now:\n";
@@ -50,47 +92,6 @@ class Command {
       return channel.sendMessage('<http://lmgtfy.com/?q=' + args.join('+') + '>');
     }
 
-    say(args, msg) {
-      var { channel } = msg;
-      if ( args.length < 1) { return channel.sendMessage(this.argsErrorMessage); }
-          let saying = args.join(" ");
-          return channel.sendMessage(saying);
-    }
-
-    search(args, msg) {
-      const { key } = config;
-      const { cx }  = config;
-      var { channel } = msg;
-      let url = `https://www.googleapis.com/customsearch/v1?key=${key}&cx=${cx}&safe=off&q=${encodeURI(args)}`;
-
-      superagent.get(url).end((err, res) => {
-          if (err) return msg.reply("superagent error...");
-          if (res.body.queries.request[0].totalResults === '0') return channel.sendMessage('`No results found.`');
-          channel.sendMessage(res.body.items[0].link).catch(() => {
-              return msg.reply("response error...");
-        });
-      });
-    }
-
-    add(args, msg) {
-      var { channel } = msg;
-      if ( args.length < 1) { return channel.sendMessage(this.argsErrorMessage); }
-      let numArray = args.map(n => parseInt(n));
-      let total = numArray.reduce((p, c) => p + c);
-      return channel.sendMessage(total);
-    }
-
-    rules(args, msg) {
-      var { channel } = msg;
-      return channel.sendMessage("Be nice and don't copy each other's homework!");
-    }
-
-    xmas(args, msg) {
-       var { channel } = msg;
-       var randomImage = "https://giphy.com/gifs/foxhomeent-3o7TKLHb0PWRNnoVq0";
-       channel.sendMessage(randomImage);
-    }
-
     purge(args, msg) {
         var { guild } = msg;
         // Make sure the person doing the command is a Board Member
@@ -117,6 +118,37 @@ class Command {
       if (!guild.member(bot.user).hasPermission("MANAGE_CHANNELS")) {
         return msg.reply("sorry m8, you're not authorized to use that command.");
       }
+    }
+
+    rules(args, msg) {
+      var { channel } = msg;
+      return channel.sendMessage("Be nice and don't copy each other's homework!");
+    }
+
+    say(args, msg) {
+      var { channel } = msg;
+      if ( args.length < 1) { return channel.sendMessage(this.argsErrorMessage); }
+          let saying = args.join(" ");
+          return channel.sendMessage(saying);
+    }
+
+    search(args, msg) {
+      const { key } = config;
+      const { cx }  = config;
+      var { channel } = msg;
+      let url = `https://www.googleapis.com/customsearch/v1?key=${key}&cx=${cx}&safe=off&q=${encodeURI(args)}`;
+
+      superagent.get(url).end((err, res) => {
+          if (err) return msg.reply("superagent error...");
+          if (res.body.queries.request[0].totalResults === '0') return channel.sendMessage('`No results found.`');
+          channel.sendMessage(res.body.items[0].link).catch(() => {
+              return msg.reply("response error...");
+        });
+      });
+    }
+
+    source(args, msg) {
+        return msg.channel.sendMessage("Hack me at https://github.com/osu-cascades/hackbot");
     }
 
     weather(args, msg) {
@@ -158,44 +190,12 @@ class Command {
             })
     }
 
-    source(args, msg) {
-        return msg.channel.sendMessage("Hack me at https://github.com/osu-cascades/hackbot");
+    xmas(args, msg) {
+       var { channel } = msg;
+       var randomImage = "https://giphy.com/gifs/foxhomeent-3o7TKLHb0PWRNnoVq0";
+       channel.sendMessage(randomImage);
     }
-
-    gitProfile(args, msg) {
-        if ( args.length < 1) { return channel.sendMessage(this.argsErrorMessage); }
-        var getGithubProfile = (userName) => {
-                return new Promise((resolve, reject) => {
-                    if (userName === undefined) {
-                        return reject('Please enter a username.');
-                    } else {
-                        let url = `https://api.github.com/users/${userName}`;
-                        let options = {
-                            url: url,
-                            headers: {
-                                'User-Agent': 'request'
-                            }
-                        }
-                        request(options, (error, response, body) => {
-                            if (error) {
-                                return reject(`error: ${error}`);
-                            } else {
-                                resolve(body);
-                            }
-                        })
-                    }
-                })
-        }
-        getGithubProfile(args)
-            .then((profile) => {
-                let parsedProfile = JSON.parse(profile);
-                msg.channel.sendMessage(parsedProfile.html_url);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
-
 
 }
+
 module.exports = Command;
