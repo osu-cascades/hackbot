cecc8ded03e4207e976990e5431572c6ceb38ca5
Yong Bakos <ybakos@humanoriented.com>
2017-08-30 17:59:10 -0700

    hackbot: Normalize quotes.

--- hackbot.js
+++ hackbot.js
@@ -1,26 +1,26 @@
-const Discord = require("discord.js");
-const config = require("./config.json");
+const Discord = require('discord.js');
+const config = require('./config.json');
 const CommandParser = require('./Commands/CommandParser');
 const bot = new Discord.Client();
 
 var cmdParser = new CommandParser(config.prefix);
 
 // Prints ready message to console
-bot.on("ready", () => {
+bot.on('ready', () => {
     console.log(`Ready to serve in ${bot.channels.size} channels on ${bot.guilds.size} servers, for a total of ${bot.users.size} users.`);
 });
 
-bot.on("message", msg => {
+bot.on('message', msg => {
     response = cmdParser.parse(msg);
     if ( response !== "" && response !== undefined ) {
-	      return response;
+         return response;
     } else {
         return;
     }
 });
 
 // Detects new users and sends them a little message.
-bot.on("guildMemberAdd", (member) => {
+bot.on('guildMemberAdd', (member) => {
     var { username } = member.user;
     console.log(`New User "${username}" has joined "${member.guild.name}"` );
     member.guild.defaultChannel.sendMessage(`"${username}" has joined this server`);
