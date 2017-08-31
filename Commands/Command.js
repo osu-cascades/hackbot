bccba50ac62840745b1698dc7598c09387b839f7
Steven Harding <sharding1012@gmail.com>
2017-08-30 19:09:26 -0700

    edited where the search commands errors are logged

--- Commands/Command.js
+++ Commands/Command.js
@@ -64,10 +64,11 @@ class Command {
       let url = `https://www.googleapis.com/customsearch/v1?key=${key}&cx=${cx}&safe=off&q=${encodeURI(args)}`;
 
       superagent.get(url).end((err, res) => {
-          if (err) return msg.reply("superagent error...");
+          if (err) { console.log("superagent error...") return; };
           if (res.body.queries.request[0].totalResults === '0') return channel.sendMessage('`No results found.`');
           channel.sendMessage(res.body.items[0].link).catch(() => {
-              return msg.reply("response error...");
+              console.log("response error...");
+              return;
         });
       });
     }
