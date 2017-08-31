3656bbb32f163215afe033de715e59d90d9c16bd
Yong Bakos <ybakos@humanoriented.com>
2017-08-31 14:48:52 -0700

    CommandParser: Bring test up to date.

--- commands/command-parser.test.js
+++ commands/command-parser.test.js
@@ -2,10 +2,8 @@ const CommandParser = require('./command-parser');
 
 test('Is valid after instantiation', () => {
   const prefix = 'FAKE';
-  const fakeCommandObject = 'Fake Command Object';
-  const parser = new CommandParser(prefix, fakeCommandObject);
+  const parser = new CommandParser(prefix);
   expect(parser.prefix).toMatch(prefix);
-  expect(parser.command).toBe(fakeCommandObject);
 });
 
 describe('Parsing messages', () => {
