25c608f1925aa89c2454e2b467d38d995f8ae360
Steven Harding <sharding1012@gmail.com>
2017-08-31 12:49:25 -0700

    Command: Log errors and humanize bot response.

--- commands/command.js
+++ commands/command.js
@@ -139,10 +139,14 @@ class Command {
       let url = `https://www.googleapis.com/customsearch/v1?key=${key}&cx=${cx}&safe=off&q=${encodeURI(args)}`;
 
       superagent.get(url).end((err, res) => {
-          if (err) return msg.reply("superagent error...");
+          if (err) {
+            console.log("superagent error...");
+            return msg.reply("Sorry, I don't seem to be able to do that...");
+          }
           if (res.body.queries.request[0].totalResults === '0') return channel.sendMessage('`No results found.`');
           channel.sendMessage(res.body.items[0].link).catch(() => {
-              return msg.reply("response error...");
+            console.log("response error...");
+            return msg.reply("Sorry, I had a problem getting a response from google.");
         });
       });
     }
