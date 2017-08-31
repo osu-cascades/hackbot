8aeba76ec48b70be0278db3ac71fc85a7efa36ee
Yong Bakos <ybakos@humanoriented.com>
2017-08-30 21:37:27 -0700

    CommandParser: Decouple from Command via consturctor injection.

--- commands/command-parser.test.js
+++ commands/command-parser.test.js
@@ -2,6 +2,8 @@ const CommandParser = require('./command-parser');
 
 test('Is valid after instantiation', () => {
   const prefix = 'FAKE';
-  const parser = new CommandParser(prefix);
+  const fakeCommandObject = 'Fake Command Object';
+  const parser = new CommandParser(prefix, fakeCommandObject);
   expect(parser.prefix).toMatch(prefix);
+  expect(parser.command).toBe(fakeCommandObject);
 });
