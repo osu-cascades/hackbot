80bfbea29424ece583dbc5b0efbc4695bbeeb43d
Yong Bakos <ybakos@humanoriented.com>
2017-09-01 09:18:56 -0700

    CommandParser test: Refactor duplication with beforeEach.

--- commands/command-parser.test.js
+++ commands/command-parser.test.js
@@ -1,49 +1,49 @@
 const CommandParser = require('./command-parser');
 
-test('Is valid after instantiation', () => {
-  const prefix = 'FAKE';
-  const parser = new CommandParser(prefix);
-  expect(parser.prefix).toMatch(prefix);
-});
-describe('Parsing messages', () => {
-  context('When the message is undefined', () => {
-    test('It returns undefined', () => {
-      const parser = new CommandParser('FAKE');
-      expect(parser.parse(undefined)).toBeUndefined();
-    });
+describe('CommandParser', () => {
+
+  const messagePrefix = 'FAKE';
+  var parser;
+
+  beforeEach(() => {
+    parser = new CommandParser(messagePrefix)
   });
-  context('When the message is null', () => {
-    test('It returns undefined', () => {
-      const parser = new CommandParser('FAKE');
-      expect(parser.parse(null)).toBeUndefined();
-    });
+
+  test('Is valid after instantiation', () => {
+    expect(parser.prefix).toMatch(messagePrefix);
   });
-  context('When the message is not something with a content property', () => {
-    test('It returns undefined', () => {
-      const parser = new CommandParser('FAKE');
-      expect(parser.parse({ fake: 'fake' })).toBeUndefined();
+  describe('Parsing messages', () => {
+    context('When the message is undefined', () => {
+      test('It returns undefined', () => {
+        expect(parser.parse(undefined)).toBeUndefined();
+      });
     });
-  });
-  context('When the message content is not a String', () => {
-    test('It returns undefined', () => {
-      const parser = new CommandParser('FAKE');
-      expect(parser.parse({ content: null })).toBeUndefined();
-      expect(parser.parse({ content: undefined })).toBeUndefined();
-      expect(parser.parse({ content: 42 })).toBeUndefined();
+    context('When the message is null', () => {
+      test('It returns undefined', () => {
+        expect(parser.parse(null)).toBeUndefined();
+      });
     });
-  });
-  context('When the message content is not prefixed', () => {
-    test('It returns undefined', () => {
-      const prefix = 'FAKE';
-      const parser = new CommandParser(prefix);
-      expect(parser.parse({ content: 'An ignorable message' })).toBeUndefined();
+    context('When the message is not something with a content property', () => {
+      test('It returns undefined', () => {
+        expect(parser.parse({ fake: 'fake' })).toBeUndefined();
+      });
     });
-  });
-  context('When the message content is prefixed', () => {
-    test('It returns a command and its arguments', () => {
-      const prefix = 'FAKE';
-      const parser = new CommandParser(prefix);
-      expect(parser.parse({ content: `${prefix}cmd` })).toMatchObject(['cmd', []]);
+    context('When the message content is not a String', () => {
+      test('It returns undefined', () => {
+        expect(parser.parse({ content: null })).toBeUndefined();
+        expect(parser.parse({ content: undefined })).toBeUndefined();
+        expect(parser.parse({ content: 42 })).toBeUndefined();
+      });
+    });
+    context('When the message content is not prefixed', () => {
+      test('It returns undefined', () => {
+        expect(parser.parse({ content: 'An ignorable message' })).toBeUndefined();
+      });
+    });
+    context('When the message content is prefixed', () => {
+      test('It returns a command and its arguments', () => {
+        expect(parser.parse({ content: `${messagePrefix}cmd` })).toMatchObject(['cmd', []]);
+      });
     });
   });
 });
