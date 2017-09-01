f8a8871f133ba849e04a4faa2ac5b49f211483d2
Yong Bakos <ybakos@humanoriented.com>
2017-09-01 11:13:22 -0700

    CommandParser: Massage test descriptions.

--- commands/command-parser.test.js
+++ commands/command-parser.test.js
@@ -39,22 +39,22 @@ describe('CommandParser', () => {
   describe('Parsing messages', () => {
 
     context('When the message is undefined', () => {
-      test('It returns undefined', () => {
+      test('it returns undefined', () => {
         expect(parser.parse(undefined)).toBeUndefined();
       });
     });
     context('When the message is null', () => {
-      test('It returns undefined', () => {
+      test('it returns undefined', () => {
         expect(parser.parse(null)).toBeUndefined();
       });
     });
     context('When the message is not something with a content property', () => {
-      test('It returns undefined', () => {
+      test('it returns undefined', () => {
         expect(parser.parse({ fake: 'fake' })).toBeUndefined();
       });
     });
     context('When the message content is not a String', () => {
-      test('It returns undefined', () => {
+      test('it returns undefined', () => {
         expect(parser.parse({ content: null })).toBeUndefined();
         expect(parser.parse({ content: undefined })).toBeUndefined();
         expect(parser.parse({ content: 42 })).toBeUndefined();
@@ -62,24 +62,24 @@ describe('CommandParser', () => {
     });
 
     context('When the message content is not prefixed', () => {
-      test('It returns undefined', () => {
+      test('it returns undefined', () => {
         expect(parser.parse({ content: 'An ignorable message' })).toBeUndefined();
       });
     });
 
     context('When the message content is prefixed', () => {
       context('and the content has many space-separated words', () => {
-        test('It returns a command and the words as arguments', () => {
+        test('it returns a command and the words as an array of arguments', () => {
           expect(parser.parse({ content: `${messagePrefix}cmd fee fi fo funk` })).toMatchObject(['cmd', ['fee', 'fi', 'fo', 'funk']]);
         });
       });
       context('and the content has no additional words', () => {
-        test('It returns a command and its arguments', () => {
+        test('it returns a command and an empty arguments array', () => {
           expect(parser.parse({ content: `${messagePrefix}cmd` })).toMatchObject(['cmd', []]);
         });
       });
-      context('But the message content only contains the prefix', () => {
-        test('It returns undefined', () => {
+      context('but the message content only contains the prefix', () => {
+        test('it returns undefined', () => {
           expect(parser.parse({ content: 'FAKE' })).toBeUndefined();
         });
       });
