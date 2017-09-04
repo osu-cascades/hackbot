bdb9bedb90b3a5713624d57db1816513764eaf82
Steven Harding <sharding1012@gmail.com>
2017-09-04 15:48:48 -0700

    format

--- library/command.js
+++ library/command.js
@@ -3,17 +3,19 @@
  * This should provide the structure that each commands should have.
  * The structure should be based off of the command-information.js
  * After this is implemented we will be able to remove command-information.js
- * 
+ *
  * @class Command
  */
 class Command {
   constructor() {
-    
+
   }
 
   static argsErrorMessage() {
     return 'Arguments are missing.\nRefer to `!help` or ask an Administrator if this error occurs.';
   }
+
 }
+
 
 module.exports = Command;
