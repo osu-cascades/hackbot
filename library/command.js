9ec543d5649651113ac9cc677e83ec7125b013b2
Cody Swartz <ctsstc@gmail.com>
2017-09-15 02:51:48 -0700

    Cleanup & Interface based Command class

--- library/command.js
+++ library/command.js
@@ -1,14 +1,25 @@
 /**
- * Provides a data structure for commands to abide by.
+ * Provides an interface for commands to inherit
  * 
  * @class Command
  */
 class Command {
 
   // Interface
+  static get description() {
+    throw `Subclass: ${this.name} does not implement description()`;
+  }
+
   static execute() {
     let className = this.name;//this.constructor.name;
-    throw className == 'Command' ? 'Cannot call execute() directly from the Command class' : `${className} does not implement execute()`;
+    let directCallException = 'Cannot call execute() directly from the Command class';
+    let implementingClassException = `Subclass: ${className} does not implement execute()`;
+    throw className == 'Command' ? directCallException : implementingClassException;
+  }
+
+  // Overridable
+  static get argsErrorMessage() {
+    return 'Arguments are missing.\nRefer to `!help` or ask an Administrator if this error occurs.';
   }
 }
 
