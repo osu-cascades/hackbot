9eb439a8c27e4027e6b3bcf3eba9b97707d10d56
Steven Harding <sharding1012@gmail.com>
2017-09-16 16:02:14 -0700

    implement tests for CommandLoader

--- __tests__/command-loader.test.js
+++ __tests__/command-loader.test.js
@@ -1,22 +1,26 @@
 const CommandLoader = require('../library/command-loader');
 
 describe('CommandLoader', () => {
-    let commandLoader;
-    beforeEach(() => {
-      commandLoader = new CommandLoader();
+  let commandLoader;
+  beforeEach(() => {
+    commandLoader = new CommandLoader();
+  });
+  const expected = [ expect.stringMatching(/\.\/commands\/\w*\.js/) ];
+
+  context('Gets all files from command directory', () => {
+    test('it should return an array of command files', () => {
+      let fakeFiles = ['./commands/fake.js'];
+      expect(commandLoader.getCommandFiles(fakeFiles))
+        .toEqual(expect.arrayContaining(expected));
     });
+  });
 
-    context('Gets all files from command directory', () => {
-      test('it should return all command files', () => {
-
-      });
+  context('Command directory empty', () => {
+    test('it should throw an error if array is empty or undefined', () => {
+      let fakeFiles = [[], undefined];
+      expect(() =>
+        fakeFiles.map(fakeFiles => commandLoader.getCommandFiles(fakeFiles)))
+          .toThrow('Command files could not be found');
     });
-
-    context('Gets all files from command directory', () => {
-      test('it should return an array of command methods', () => {
-
-      });
-    });
-
-
-})
+  });
+});
--- library/command-loader.js
+++ library/command-loader.js
@@ -3,17 +3,23 @@ const path = require('path');
 const camelCase = require('lodash.camelcase');
 
 class CommandLoader {
-
   constructor() {
     this.commandClasses = {};
-    this.commandFiles = this.getCommandFiles();
-    
+
+    // All of the files in commands except _template.js
+    this.getFiles = glob.sync('./commands/**/*.js').filter(file => file != './commands/_template.js');
+    this.commandFiles = this.getCommandFiles(this.getFiles);
     this.loadCommandClasses();
   }
 
-  getCommandFiles() {
-    // All of the files in commands except _template.js
-    return glob.sync('./commands/**/*.js').filter(file => file != './commands/_template.js');
+  getCommandFiles(commandFiles) {
+    if (
+      commandFiles === undefined ||
+      commandFiles.length === 0
+    ) {
+      throw 'Command files could not be found';
+    }
+    return commandFiles;
   }
 
   loadCommandClasses() {
@@ -21,12 +27,13 @@ class CommandLoader {
     // Load all commands in the commands folder besides _template.js
     this.commandFiles.forEach(function(file) {
       let key = path.basename(file, path.extname(file));
+
       // Convert the kebab file names to camel case
       key = camelCase(key);
       this.commandClasses[key] = require(path.resolve(file));
+
     }, this);
   }
-
 }
 
 module.exports = CommandLoader;
