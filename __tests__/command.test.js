f5550aeb3c87ec0e774d4d8ffbdb6e68bc936f84
Yong Bakos <ybakos@humanoriented.com>
2017-09-18 21:16:10 -0700

    Command: Add comments and test.

---
+++ __tests__/command.test.js
@@ -0,0 +1,35 @@
+const Command = require('../library/command');
+
+describe('Command', () => {
+
+  describe('Instantiation', () => {
+    test('should raise an exception', () => {
+      expect(() => {
+        new Command();
+      }).toThrow();
+    });
+  });
+
+  describe('description', () => {
+    test('should raise an exception', () => {
+      expect(() => {
+        Command.description;
+      }).toThrow();
+    });
+  });
+
+  describe('execute', () => {
+    test('should raise an exception', () => {
+      expect(() => {
+        Command.execute('fake', 'fake');
+      }).toThrow();
+    });
+  });
+
+  describe('argsErrorMessage', () => {
+    test('should be an error message', () => {
+      expect(Command.argsErrorMessage).toMatch('Arguments are missing.\nRefer to `!help` or ask an Administrator if this error occurs.');
+    });
+  });
+
+});
--- library/command.js
+++ library/command.js
@@ -1,31 +1,43 @@
 /**
- * Provides an interface for commands to inherit
- * 
+ * An interface for all commands to extend, representing the API that all
+ * subclasses should implement.
+ *
  * @class Command
  */
 class Command {
 
-  // Interface
+  /**
+   * Prevent instantiation.
+   */
+  constructor() {
+    throw 'Do not instantiate a command. Rely on its static methods.'
+  }
+
+  /**
+   * A description of the command. Subclasses should implement this property.
+   */
   static get description() {
     throw `Subclass: ${this.name} does not implement description()`;
   }
 
-  static execute() {
-    let className = this.name;//this.constructor.name;
+  /**
+   * Execute the command. Subclasses should implement this method.
+   */
+  static execute(args, msg) {
+    let className = this.name;
     let directCallException = 'Cannot call execute() directly from the Command class';
     let implementingClassException = `Subclass: ${className} does not implement execute()`;
     throw className == 'Command' ? directCallException : implementingClassException;
   }
 
-  // Overridable
+  /**
+   * An inheritable error message for reporting a problem with command
+   * arguments. Subclasses may override this with a more specific message.
+   */
   static get argsErrorMessage() {
     return 'Arguments are missing.\nRefer to `!help` or ask an Administrator if this error occurs.';
   }
 
-  // TODO: implement args
-  //  Help will read these out
-  //  The executor can validate length automatically
-  //  Must support optional & varying length of args: {state: true, city: false, _additional: true}
 }
 
 module.exports = Command;
