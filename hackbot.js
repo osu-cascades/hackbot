a76f17f3086857a34fd6e96b4a06ce3dc4ae6a41
ruko <rukokarasu@gmail.com>
2017-09-22 11:07:53 -0700

    Typo in console log msg

--- hackbot.js
+++ hackbot.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
     if (general) {
       general.sendMessage(`'${username}' has joined this server`);
     } else {
-      console.log("This server has no defaultChannel property. (It might be too new. If you are running the bot locally, please specify the default channel in .env.");
+      console.log("This server has no defaultChannel property. (It might be too new.) If you are running the bot locally, please specify the default channel in .env.");
     }
   });
 }
