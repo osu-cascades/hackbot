4bfa797ae7ebf243a81ea29c7a1035142b6302ac
Cody Swartz <ctsstc@gmail.com>
2018-04-20 23:05:05 -0700

    Impelments version command
    Displays the current running environment and running version from the package.json file

---
+++ commands/version.js
@@ -0,0 +1,24 @@
+const Command = require('../library/command');
+const { version } = require('../package.json');
+
+/**
+ * @class Version
+ * @extends {Command}
+ */
+
+class Version extends Command {
+
+  static get description() {
+    return 'Gets the bot\'s current running version from package.json';
+  }
+
+  static execute(args, msg) {
+    const { channel } = msg;
+    let nodeEnv = process.env.NODE_ENV;
+    let runningEnv = nodeEnv ? `${nodeEnv} ` : '';
+    return channel.sendMessage(`Hackbot ${runningEnv}is running v${version}`);
+  }
+
+}
+
+module.exports = Version;
