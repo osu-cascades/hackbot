43640176710b8d9ea50ffafd846799bb6b9a9856
Cody Swartz <ctsstc@gmail.com>
2018-04-21 00:51:44 -0700

    Cleans up Command Loader and Adds Tests
    Command loader is now just static methods.
    I no longer saw a need for an instance of it.
    
    Added a test to check that the private static method `_removeTemplateFile` removes the template file.
    
    Checks that there's a template file in the commands directory.

--- library/command-loader.js
+++ library/command-loader.js
@@ -2,29 +2,24 @@ const path = require('path');
 const camelCase = require('lodash.camelcase');
 
 class CommandLoader {
-  constructor(commandClassFiles) {
-    this.commandClasses = {};
-    this.commandClassFiles = commandClassFiles;
-    this.loadCommandClasses();
-  }
 
-  getCommandFiles() {
-    return this.commandClassFiles;
+  static _removeTemplateFile(files) {
+    return files.filter(file => file != './commands/_template.js');
   }
-
-  loadCommandClasses() {
+  static getCommandClasses(commandClassFiles) {
     /**
      * https://stackoverflow.com/questions/5364928/node-js-require-all-files-in-a-folder
      * Load all commands in the commands folder besides _template.js
      */
-    this.getCommandFiles().forEach(function(file) {
+    let files = CommandLoader._removeTemplateFile(commandClassFiles);
+    return files.reduce((prev, file) => {
       let key = path.basename(file, path.extname(file));
 
       // Convert the kebab file names to camel case
       key = camelCase(key);
-      this.commandClasses[key] = require(path.resolve(file));
-    }, this);
-    return this.commandClasses;
+      prev[key] = require(path.resolve(file));
+      return prev;
+    }, {});
   }
 }
 
