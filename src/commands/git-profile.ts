46f2cc147545143c90c030473ffb34c239d8f654
Cody Swartz <ctsstc@gmail.com>
2018-09-04 00:49:16 -0700

    Fix git profile command

--- src/commands/git-profile.ts
+++ src/commands/git-profile.ts
@@ -20,15 +20,14 @@ export default GitProfile = class {
     }
     else {
       this.getGithubProfile(args[0])
-      .then((profile: string) => {
-        const parsedProfile = JSON.parse(profile);
-        return channel.send(parsedProfile.html_url);
+      .then((profile: GitProfileResponse) => {
+        return channel.send(profile.html_url);
       })
       .catch(console.error);
     }
   }
 
-  private static getGithubProfile(userName: string): Promise<string> {
+  private static getGithubProfile(userName: string): Promise<GitProfileResponse> {
     const options = {
       url: `https://api.github.com/users/${userName}`,
       headers: {
@@ -41,4 +40,38 @@ export default GitProfile = class {
     });
   }
 
+}
+
+export type GitProfileResponse = {
+  login: string,
+  id: number,
+  node_id: string,
+  avatar_url: string,
+  gravatar_id: string,
+  url: string,
+  html_url: string,
+  followers_url: string,
+  following_url: string,
+  gists_url: string,
+  starred_url: string,
+  subscriptions_url: string,
+  organizations_url: string,
+  repos_url: string,
+  events_url: string,
+  received_events_url: string,
+  type: string,
+  site_admin: boolean,
+  name: string,
+  company: string,
+  blog: string,
+  location: string,
+  email: string|null,
+  hireable: boolean,
+  bio: string|null,
+  public_repos: number,
+  public_gists: number,
+  followers: number,
+  following: number,
+  created_at: string,
+  updated_at: string
 }
