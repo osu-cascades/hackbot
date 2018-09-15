03f84a9db142683ce14ad51677e40b527ba976d5
Cody Swartz <ctsstc@gmail.com>
2018-09-15 16:17:21 -0700

    Fix failing spec - Don't test private methods

--- __tests__/library/command-loader.test.ts
+++ __tests__/library/command-loader.test.ts
@@ -1,30 +1,14 @@
-import CommandLoader, { CommandClasses } from '../../src/library/command-loader';
 import glob from 'glob';
+import CommandLoader, { ICommandClasses } from '../../src/library/command-loader';
 
 describe('CommandLoader', () => {
-  let commandClasses: CommandClasses;
+  let commandClasses: ICommandClasses;
   const expected = [ expect.stringMatching(/\.\/src\/commands\/\w*\.ts/) ];
-  let files = glob.sync('./src/commands/**/*.ts');
+  const files = glob.sync('./src/commands/**/*.ts');
   const templateFile = './src/commands/_template.ts';
 
   beforeEach(() => {
     commandClasses = CommandLoader.getCommandClasses(files);
-  });
-
-  describe('Gets all files from command directory', () => {
-    let commandFiles = CommandLoader._removeTemplateFile(files);
-    test('it should return an array of command classes', () => {
-      expect(commandFiles)
-        .toEqual(expect.arrayContaining(expected));
-    });
-    test('it should have a template file', () => {
-      expect(files)
-        .toEqual(expect.arrayContaining([templateFile]));
-    });
-    test('it should filter the template file', () => {
-      expect(commandFiles)
-        .not.toEqual(expect.arrayContaining([templateFile]));
-    });
   });
 
   describe('Return type should be an object', () => {
@@ -35,8 +19,8 @@ describe('CommandLoader', () => {
 
   describe('Class names match their file names', () => {
     test('they match their key name', () => {
-      for (let commandName in commandClasses) {
-        let commandClass = commandClasses[commandName];
+      for (let commandName of Object.keys(commandClasses)) {
+        const commandClass = commandClasses[commandName];
         // Capitalize first character
         commandName = commandName.replace(/^\w/, (char) => {
           return char.toUpperCase();
