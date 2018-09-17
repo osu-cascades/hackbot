62c0c10e92c81cc4b9b3631e21c1629cc8b7d9f2
Cody Swartz <ctsstc@gmail.com>
2018-09-17 00:35:15 -0700

    Tests for Rules Command

--- src/commands/rules.ts
+++ src/commands/rules.ts
@@ -11,8 +11,7 @@ export default Rules = class {
   }
 
   public static execute(args: string[], msg: Message) {
-    const { channel } = msg;
-    return channel.send("Be nice and don't copy each other's homework!");
+    return msg.channel.send("Be nice and don't copy each other's homework!");
   }
 
 };
