6cd77a6c46c025c33bf1df82d5fe0930bef0ac9f
Cody Swartz <ctsstc@gmail.com>
2018-09-16 18:07:19 -0700

    Simplify version command The environment will now always be something

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -13,9 +13,7 @@ export default Version = class {
   }
 
   public static execute(args: string[], msg: Message) {
-    const { channel } = msg;
-    const runningEnv = config.env ? `${config.env} ` : '';
-    return channel.sendMessage(`Hackbot ${runningEnv}is running v${version}`);
+    return msg.channel.sendMessage(`Hackbot ${config.env} is running v${version}`);
   }
 
 };
