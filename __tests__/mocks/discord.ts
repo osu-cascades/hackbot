45d8bd25566efd6f2a4fd6b1055fc10cded243f7
Cody Swartz <ctsstc@gmail.com>
2018-11-06 00:56:37 -0800

    The easy way out...

--- __tests__/mocks/discord.ts
+++ __tests__/mocks/discord.ts
@@ -7,6 +7,6 @@ export const client = new Client();
 export const guild = new Guild(client, {});
 export const textChannel = new TextChannel(guild, {});
 export const message = new Message(textChannel, false as any, client);
-export type MockedMessage = jest.MockInstance<Promise<Message|Message[]>>;
+export type MockedMessage = jest.MockInstance<Promise<Message|Message[]>> | any;
 
 message.channel = textChannel;
