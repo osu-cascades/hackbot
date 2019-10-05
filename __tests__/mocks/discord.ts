2aad8a249edcf2d6dd7d8b7345011da74ed3de07
Cody Swartz <ctsstc@gmail.com>
2019-10-05 01:50:53 -0700

    Revert Mocked Message to master
    I got my local specs passing,,,
    but then I forgot to update dependencies

--- __tests__/mocks/discord.ts
+++ __tests__/mocks/discord.ts
@@ -6,6 +6,6 @@ export const client = new Client();
 export const guild = new Guild(client, {});
 export const textChannel = new TextChannel(guild, {});
 export const message = new Message(textChannel, false as any, client);
-export type MockedMessage = jest.MockInstance<Promise<Message|Message[]>> | any;
+export type MockedMessage = jest.MockInstance<Promise<Message|Message[]>, any> | any;
 
 message.channel = textChannel;
