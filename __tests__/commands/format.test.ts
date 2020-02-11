04ef57d3f127f1266a07ac4cd640a586f7240ed1
Johnny Phelan <phelanjo@oregonstate.edu>
2020-02-10 16:31:43 -0800

    Provide test for new !format command

---
+++ __tests__/commands/format.test.ts
@@ -0,0 +1,22 @@
+import Format from '../../src/commands/format';
+
+import { message as mockMessage, MockedMessage } from '../mocks/discord';
+
+let sendMock: MockedMessage;
+beforeEach(() => {
+  sendMock = jest.fn();
+  mockMessage.channel.send = sendMock;
+});
+
+describe('Format Command', () => {
+  test('provides code formatting tip', () => {
+    Format.execute([], mockMessage);
+    expect(sendMock.mock.calls[0][0]).toContain(
+      'To format code snippets on discord, use triple-backticks and a language name.\n' +
+        'For example:\n' +
+        '\\```dart\nvoid function(type argument) {\n\treturn values;\n}\n\\```\n' +
+        'Becomes:\n' +
+        '```dart\nvoid function(type argument) {\n  return values;\n}```'
+    );
+  });
+});
