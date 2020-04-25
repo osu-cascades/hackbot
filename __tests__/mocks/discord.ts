48613c8d5defe4ce9cf57b3db44521b7cba474a7
Cody Swartz <ctsstc@gmail.com>
2020-04-25 06:58:49 -0700

    Edit: change the discord main mock
    This is how it should really be, not off instanciating junk.
    
    The missing link was the TS `as` keyword ;)
    
    Also hopefully simplifying the verbiage

--- __tests__/mocks/discord.ts
+++ __tests__/mocks/discord.ts
@@ -1,11 +1,12 @@
-import { Client, Guild, Message, TextChannel } from 'discord.js';
+import {  Message } from 'discord.js';
 
 jest.mock('discord.js');
 
-export const client = new Client();
-export const guild = new Guild(client, {});
-export const textChannel = new TextChannel(guild, {});
-export const message = new Message(textChannel, false as any, client);
-export type MockedMessage = jest.MockInstance<Promise<Message|Message[]>, any> | any;
+// A snazzy Fixture™
+export const mockMessage = {
+  channel: {
+    send: {}
+  }
+} as Message;
 
-message.channel = textChannel;
+export type MockMessage = jest.MockInstance<Promise<Message|Message[]>, any> | any;
